9edc42bab094ff09ccb8d1146e348f55d33f1e74
nikolav <vukovicnikola2014@gmail.com>
2021-06-06 20:56:05 +0100

    installs sqlite3

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 
-var express = require('express');
-
-
 var app;
 var server;
+var express;
+var sqlite3;
+
+express = require('express');
+sqlite3 = require('sqlite3');
+
 
 app = express();
+app.set('view engine', 'ejs');
+
 app.get(
     '/', 
     function (req, res) {
-      res.send('<section><h1>hola mundo</h1><article>express js</article></section>');
+      // res.send('<section><h1>hola mundo</h1><article>express js</article></section>');
+      res.render('default');
+
     }
 );
+
+
 server = app.listen(3000, function () {
   console.log('listening on http://localhost:3000')
 });
