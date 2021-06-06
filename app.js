91359758d62e26fcfa31f77dcbae09b93e383f12
nikolav <vukovicnikola2014@gmail.com>
2021-06-06 17:07:40 +0100

    listen @port:3000

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-var express = import('express');
+
+var express = require('express');
+
+
+var app;
+var server;
+
+app = express();
+app.get(
+    '/', 
+    function (req, res) {
+      res.send('<section><h1>hola mundo</h1><article>express js</article></section>');
+    }
+);
+server = app.listen(3000, function () {
+  console.log('listening on http://localhost:3000')
+});
 
 console.log('app running');
+//eof
